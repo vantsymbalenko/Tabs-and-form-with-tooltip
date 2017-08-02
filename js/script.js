594c3ee261558409317f5f7539fb5db0d58c02ca
vantsymbalenko <IvanZymbalenko1@gmail.com>
2017-08-02 09:50:05 +0300

    Add form with tooltip

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(function() {
 	var $div = $(".content div");
 	var $li = $(".tabs li");
+	var $tooltip = $(".hint div");
 	$div.hide();
 	function start() {
 		$li.eq(0).addClass('active');
@@ -8,9 +9,15 @@ $(function() {
 	}
 	start();
 	$li.on("click", function() {
-			$li.removeClass('active');
-			$(this).addClass('active');
-			$div.hide();
-			$div.eq($(this).index()).show();
-		});
+		$li.removeClass('active');
+		$(this).addClass('active');
+		$div.hide();
+		$div.eq($(this).index()).show();
+	});
+// show tooltip
+	$tooltip.hover(function() {
+		$(this).find("span").show();
+	}, function() {
+		$(this).find("span").hide();
+	});
 });
